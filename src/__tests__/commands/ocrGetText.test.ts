2c97d53b7ace74daa56506185a8411169667c152
Wim Selles <wim@wswebcreation.nl>
2021-04-05 09:05:18 +0200

    test: add ocrGetText, ocrSetValue and ocrWaitForTextDisplayed tests

---
+++ src/__tests__/commands/ocrGetText.test.ts
@@ -0,0 +1,18 @@
+import ocrGetText from '../../commands/ocrGetText'
+
+jest.mock('../../utils/ocrGetData', ()=> jest.fn().mockResolvedValue({
+  text: 'Username\n\nPassword\n\n \n\n \n\nLOGIN\n\n \n\n \n',
+}))
+
+describe('ocrGetText', () => {
+  it('should be able to return text', async () => {
+    const options = {
+      isTesseractAvailable: true,
+      ocrImagesPath: 'ocrImagesPath',
+      reuseOcr: true,
+      screenSize: { width: 1, height: 2 },
+    }
+
+    expect(await ocrGetText(options)).toEqual('Username\nPassword\nLOGIN\n')
+  })
+})
---
+++ src/__tests__/commands/ocrSetValue.test.ts
@@ -0,0 +1,33 @@
+import ocrSetValue from '../../commands/ocrSetValue'
+import ocrKeys from '../../utils/ocrKeys'
+import ocrClickOnText from '../../commands/ocrClickOnText'
+
+jest.mock('../../utils/ocrKeys')
+jest.mock('../../commands/ocrClickOnText')
+
+describe('ocrSetValue', () => {
+  it('should be able to set a value', async () => {
+    const options = {
+      isTesseractAvailable: true,
+      ocrImagesPath: 'ocrImagesPath',
+      reuseOcr: true,
+      screenSize: { width: 1, height: 2 },
+      text: 'text',
+      value: 'value'
+    }
+    const globalAny:any = global
+    globalAny.driver = {
+      hideKeyboard: jest.fn(),
+      waitUntil: jest.fn().mockImplementation((fn)=>fn()),
+      isKeyboardShown: jest.fn(),
+    }
+
+    await ocrSetValue(options)
+
+    expect(await ocrClickOnText).toHaveBeenCalledWithSnapshot()
+    expect(await globalAny.driver.waitUntil).toHaveBeenCalledTimes(2)
+    expect(await globalAny.driver.isKeyboardShown).toHaveBeenCalledTimes(2)
+    expect(await ocrKeys).toHaveBeenCalledWith(options.value)
+    expect(await globalAny.driver.hideKeyboard).toHaveBeenCalledTimes(1)
+  })
+})
